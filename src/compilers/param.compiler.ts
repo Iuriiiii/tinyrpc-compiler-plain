ab672318ba48be0d3c9fc8fd9f4c3c87b0529102
Iuriiiiiii <alex.casas@webtroniclabs.com>
2024-11-21 23:50:30 -0300

    feat/compile-constructor

--- src/compilers/param.compiler.ts
+++ src/compilers/param.compiler.ts
@@ -1,7 +1,7 @@
 import { getTypescriptType } from "../utils/mod.ts";
 import type { CompilerOptions, MemberMetadata } from "@tinyrpc/server";
 
-export function buildMember(
+export function paramCompiler(
   member: MemberMetadata,
   buildImports: string[],
   options: CompilerOptions,
@@ -9,26 +9,19 @@ export function buildMember(
   const {
     dataType,
     name: memberName,
-    optional,
-    defaultValue,
     private: isPrivate,
     nullable,
-    readonly,
   } = member;
   const { typescriptType: buildType, requireImport } = getTypescriptType(
     dataType,
     options.metadata,
   );
-  const makeOptional = optional ? "?" : "";
-  const makeDefaultValue = defaultValue !== undefined ? ` = ${defaultValue}` : "";
   const makePrivate = isPrivate ? "private " : "public ";
   const makeNullable = nullable ? " | null" : "";
-  const makeLateInit = defaultValue === undefined && !optional ? "!" : "";
-  const makeReadonly = readonly ? "readonly " : "";
 
   if (requireImport && !buildImports.includes(buildType)) {
     buildImports.push(buildType);
   }
 
-  return `${makePrivate}${makeReadonly}${memberName}${makeOptional}${makeLateInit}: ${buildType}${makeDefaultValue}${makeNullable}`;
+  return `${makePrivate}readonly${memberName}: ${buildType}${makeNullable}`;
 }
