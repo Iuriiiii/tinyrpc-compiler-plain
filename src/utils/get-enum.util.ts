cc58e3b8b19b1a3c40396d1cfc23fb41420265b6
Iuriiiiiii <alex.casas@webtroniclabs.com>
2024-12-08 14:35:14 -0300

    unwrappables

--- src/utils/get-enum.util.ts
+++ src/utils/get-enum.util.ts
@@ -2,5 +2,7 @@ import type { ServerMetadata } from "@tinyrpc/server/types";
 import { isString } from "@online/is";
 
 export function getEnum(value: object | string, instances: ServerMetadata) {
-  return instances.enums.find(({ name: enumName, value: enumerator }) => isString(value) ? value === enumName : value === enumerator);
+  return instances.enums.find(({ name: enumName, value: enumerator }) =>
+    isString(value) ? value === enumName : value === enumerator
+  );
 }
--- src/utils/push-type-if-needed.util.ts
+++ src/utils/push-type-if-needed.util.ts
@@ -3,7 +3,11 @@ import type { GetTypescriptTypeResult, Import } from "../interfaces/mod.ts";
 import { importContains } from "./import-contains.util.ts";
 import { pushImport } from "./push-import.util.ts";
 
-export function pushTypeIfNeeded({ tsType: type, requireImport }: GetTypescriptTypeResult, imports: Import[], options: CompilerOptions) {
+export function pushTypeIfNeeded(
+  { tsType: type, requireImport }: GetTypescriptTypeResult,
+  imports: Import[],
+  options: CompilerOptions,
+) {
   if (requireImport && !importContains(imports, type)) {
     pushImport(type, imports, options);
   }
